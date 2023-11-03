5085e50c7eaba6610d031c8b2668b9690d943eaa
Alukard <kirill1974111@gmail.com>
2023-11-03 19:58:06 +0100

    2#

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import './styles/styles.css'
 import img from "@Assets/logo.png"
 import xml from "@Assets/data.xml"
 import csv from "@Assets/data.csv"
+import ico from "@Assets/favicon.ico"
+
+import $ from "jquery"
 
 const post = new Post("Kiryl Shauchenka", img)
 
 console.log("Post to string: ", post.toString())
 console.log(xml)
 console.log(csv)
+
+$('pre').html(post.toString())
+
+$("#favicon").css("background-image", `url(${ico})`);
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,21 @@
 // npm i -D css-loader style-loader
 // npm i -D file-loader
 // npm i -D xml-loader csv-loader
+// npm i -D webpack-dev-server
+// при запуске webpack-dev-server папка dist станет ПУСТОЙ т.к все файлы будут записаны в оперативную память
+// npm i -D copy-webpack-plugin
+// npm i -D mini-css-extract-plugin
+// npm i -D css-minimizer-webpack-plugin
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin  = require('css-minimizer-webpack-plugin')
+
+const isDev = process.env.NODE_ENV
+const isProd = !isDev
+
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -18,6 +30,16 @@ module.exports = {
         filename: '[name].[contenthash].bundle.js',
         path: path.resolve(__dirname,'dist')
     },
+    optimization: {
+        splitChunks:{
+            chunks: 'all'
+        },
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin()]
+    },
+    devServer:{
+        port: 3000
+    },
     resolve:{
         alias:{
             "@Fonts" : path.resolve(__dirname, "src/assets/fonts"),
@@ -29,16 +51,28 @@ module.exports = {
             title: "Webpack",
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns:[
+                {
+                    from: path.resolve(__dirname, "src/assets/favicon.ico"),
+                    to: path.resolve(__dirname, "dist") 
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].bundle.css'
+        })
     ],
     module:{
         rules:[
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                // use: ['style-loader', 'css-loader'] - так бы мы писали если бы не было MiniCssExtractPlugin и не хотели бы выносить CSS в отделный файл
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
-                test: /\.(jpeg|png|gif|jpg)$/,
+                test: /\.(jpeg|png|gif|jpg|ico)$/,
                 type: 'asset/resource'
             },
             {
